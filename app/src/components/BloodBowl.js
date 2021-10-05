58c3674a11bb8e1959efe60bd1cf2d9380ff9ed5
irtep <rasanen.petri@gmail.com>
2021-10-05 12:07:55 +0300

    new stuff

--- app/src/components/BloodBowl.js
+++ app/src/components/BloodBowl.js
@@ -1,5 +1,5 @@
 import { drawBBfield } from '../functions/bloodBowl';
-import { initialBloodBowlObject } from '../constants/constants';
+import { initialBloodBowlObject, rerollPrices } from '../constants/constants';
 import { useEffect, useState } from 'react';
 import { getAll } from '../services/dbControl';
 import ShowAllPlayers from './ShowAllPlayers';
@@ -29,6 +29,7 @@ const BloodBowl = ({game}) => {
   const addFunc = (e) => {
     const clickedEntry = Number(e.target.id);
     let activeRoster = [];
+    const copyOfgameObject = JSON.parse(JSON.stringify(gameObject));
 
     if (activeTeam === 'Team 1') {
       activeRoster = activeRoster.concat(roster1);
@@ -40,15 +41,17 @@ const BloodBowl = ({game}) => {
     activeRoster.push(selectedPlayer[0]);
 
     if (activeTeam === 'Team 1') {
+      copyOfgameObject.team1.value += Number(selectedPlayer[0].cost);
       setRoster1(activeRoster);
     } else {
+      copyOfgameObject.team2.value += Number(selectedPlayer[0].cost);
       setRoster2(activeRoster);
     }
+    setGameObject(copyOfgameObject);
   }
 
   // sets team 1 or 2 to as active team, for player adding
   const activateTeam = (e) => {
-    console.log('e: ', e.target.id);
     if (e.target.id === 'activateTeam1') {
       setActiveTeam('Team 1');
     } else {
@@ -60,22 +63,142 @@ const BloodBowl = ({game}) => {
   const checki = () => {
     console.log('roster 1', roster1);
     console.log('roster 2', roster2);
+    console.log('gameObject ', gameObject);
   }
 
+  // game control buttons
+  const turnToggle = (e) => {
+    const copyOfgameObject = JSON.parse(JSON.stringify(gameObject));
+    let toggling = 'team1';
+
+    if (activeTeam === "Team 2") {
+      toggling = 'team2';
+    }
+
+    if (e.target.id === 'turnPlus') {
+      copyOfgameObject[toggling].turn++;
+    } else {
+      copyOfgameObject[toggling].turn--;
+    }
+
+    setGameObject(copyOfgameObject);
+  }
+  const rerollToggle = (e) => {
+    const copyOfgameObject = JSON.parse(JSON.stringify(gameObject));
+    let toggling = 'team1';
+
+    if (activeTeam === "Team 2") {
+      toggling = 'team2';
+    }
+
+    if (e.target.id === 'rerollPlus') {
+      let rerollCost = 0;
+      copyOfgameObject[toggling].rerolls++;
+      // add value of reroll to teams value
+      rerollPrices.forEach((item, i) => {
+        if (item.team === copyOfgameObject[toggling].team) {
+          console.log('found the team ', item.team, item.price);
+          rerollCost = item.price;
+        }
+      });
+      copyOfgameObject[toggling].value += rerollCost;
+    } else {
+      copyOfgameObject[toggling].rerolls--;
+    }
+
+    setGameObject(copyOfgameObject);
+  }
+  const scoreToggle = (e) => {
+    const copyOfgameObject = JSON.parse(JSON.stringify(gameObject));
+    let toggling = 'team1';
+
+    if (activeTeam === "Team 2") {
+      toggling = 'team2';
+    }
+
+    if (e.target.id === 'scorePlus') {
+      copyOfgameObject[toggling].score++;
+    } else {
+      copyOfgameObject[toggling].score--;
+    }
+
+    setGameObject(copyOfgameObject);
+  }
+  const toggleTeam = (e) => {
+    const copyOfgameObject = JSON.parse(JSON.stringify(gameObject));
+    let toggling = 'team1';
+
+    if (activeTeam === "Team 2") {
+      toggling = 'team2';
+    }
+    copyOfgameObject[toggling].team = e.target.id;
+    setGameObject(copyOfgameObject);
+  }
+  const popPlayer = (e) => {
+    const copyOfgameObject = JSON.parse(JSON.stringify(gameObject));
+    if (activeTeam === "Team 1") {
+      if (roster1.length > 0) {
+        copyOfgameObject.team1.value -= roster1[roster1.length-1].cost;
+        roster1.pop();
+      }
+    } else {
+      if (roster2.length > 0) {
+        copyOfgameObject.team2.value -= roster1[roster2.length-1].cost;
+        roster2.pop();
+      }
+    }
+    setGameObject(copyOfgameObject);
+  }
+  
   return(
     <div style= {style}>
+
       <div>
+      <button id= "Imperial Nobility" onClick= {toggleTeam}>
+        Imperial Nobility
+      </button>
+      <button id= "Black Orc" onClick= {toggleTeam}>
+        Black Orc
+      </button>
+      <button id= "Dwarf" onClick= {toggleTeam}>
+        Dwarf
+      </button>
+      <br/>
+      <button onClick= {checki}>
+        checki
+      </button>
+      <button onClick= {popPlayer}>
+        delete latest player
+      </button>
       <button id= "activateTeam1" onClick= {activateTeam}>
         team1
       </button>
       <button id= "activateTeam2" onClick= {activateTeam}>
         team2
       </button>
-      <button onClick= {checki}>
-        checki
+      <br/>
+      <button id= "turnPlus" onClick= {turnToggle}>
+        turn+
+      </button>
+      <button id= "turnMinus" onClick= {turnToggle}>
+        turn-
+      </button>
+      <button id= "rerollPlus" onClick= {rerollToggle}>
+        reroll+
+      </button>
+      <button id= "rerollMinus" onClick= {rerollToggle}>
+        reroll-
+      </button>
+      <button id= "scorePlus" onClick= {scoreToggle}>
+        score+
+      </button>
+      <button id= "scoreMinus" onClick= {scoreToggle}>
+        score-
       </button>
       <div>
-        Activated team: {activeTeam}
+        Activated team: {activeTeam}<br/>
+        {gameObject.team1.team}. score: {gameObject.team1.score} turn: {gameObject.team1.turn} rerolls: {gameObject.team1.rerolls} value: {gameObject.team1.value}<br/>
+        {gameObject.team2.team}. score: {gameObject.team2.score} turn: {gameObject.team2.turn} rerolls: {gameObject.team2.rerolls} value: {gameObject.team2.value}
       </div>
       </div>
       <div id= "canvasPlace">
--- app/src/constants/constants.js
+++ app/src/constants/constants.js
@@ -9,12 +9,22 @@ export const gameOptions = [
 export const initialBloodBowlObject = {
   team1: {
     rerolls: 0,
-    team: 'Pirates',
-    value: 0
+    team: 'Imperial Nobility',
+    value: 0,
+    score: 0,
+    turn: 0
   },
   team2: {
     rerolls: 0,
-    team: 'Predators',
-    value: 0
+    team: 'Black Orc',
+    value: 0,
+    score: 0,
+    turn: 0
   },
 };
+
+export const rerollPrices = [
+  {team : 'Imperial Nobility', price: 70000},
+  {team : 'Black Orc', price: 60000},
+  {team : 'Dwarf', price: 50000}
+];
