c96bcc58d34ec4eb31371c8444fd46e0fab7bf5c
irtep <rasanen.petri@gmail.com>
2021-10-15 14:18:39 +0300

    start of warmachine part

--- bugsEtc.js
+++ bugsEtc.js
@@ -1,10 +1,17 @@
 /*
-When clicking first time get "empty fields", clicking again will send.
-in setup adding bb card
+blood bowl bug:
+ it is possible to move one over other causing that they will be forever like that
 
-Gotta make dynamic get for factions, now hard coded buttons...
+ warmachine notes:
+   cards:
+   faction (maybe array for multifactions), stats: spd, str, mat, rat, def, arm, cmd, ctrl, base(as
+ i want these maybe in inches: 30mm = 1.18, 40mm, 1.57, 50mm= 1.97, 120mm= 4.72)
+   weapons, hitpoints or box or circle, abilities, img
+   cost
 
-Gotta filter cards by Faction... or too messy draft
+   field:
+   size should be 4f x 4f / 1200mm x 1200mm / 48 inch x 48 inch (gotta think some conversion,
+   propably 48 x 48 boxes should be correct enough and would be easy to calculate by that)
+   1 inch = 25.4mm 
 
-i think setup should have team making and they just could be loaded from blood bowl etc.
 */
