7632738dd06a56f26ca8617b2b613277ffb3f006
irtep <rasanen.petri@gmail.com>
2021-11-12 16:27:40 +0200

    bb2 design started

---
+++ app/src/components/BloodBowl2.js
@@ -0,0 +1,35 @@
+/*
+grid need to be id:d by x and y.
+
+need to convert current player cards to more divisioned,
+could convert it onLoad or change them at db
+
+need to be clear input/output system to support the ai
+
+at gameObject:
+- positions of all players in x and y system
+- number of half, number of turn,
+
+gamePlay:
+- could maybe skip fans, kick-off and weather? (or not?)
+- console were done actions would be logged
+- dice face off to who will start,
+- if human, then ai sets defence, etc..
+then click "phase ready"
+- kick off shoot "select where to kick", then selected, that
+is marked and then "phase ready"
+- maybe then option to make kick-off event?
+- offensive turn
+- click to activate, then asks "choose action"
+that is then chosen and you need to start move or choose target
+
+at constants:
+- list of "when activated" rules, for example "Bonehead" and list of who
+are affected"
+
+The GameBrain
+input that is gameObject
+- some kind of machine learning web 
+export that is action
+
+*/
